48dda92c0f4b44064c075e5dc6b371562dc4cd88
sweden <ray.likes.pasta@gmail.com>
2023-08-20 11:54:18 +0900

    theme func

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,23 @@
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import MainVisual from "../components/MainVisual";
+import MainVisual, { mainVisuals } from "../components/MainVisual";
 import MainContent from "../components/MainContent";
+
+const themes = {
+  visualTop: {
+    1: "/58787b52-38a8-4379-85c7-e61bb7f846c2.jpg",
+    2: "/circle_img1.jpg",
+    3: "/lead_img01.jpg",
+  }
+}
 
 export default function Page() {
   return (
     <>
 
       <MainVisual 
-        visualTheme="visualTop"
+        visualTheme={themes.visualTop}
       />
 
       <MainContent />
--- components/MainVisual.tsx
+++ components/MainVisual.tsx
@@ -1,8 +1,10 @@
-export default function MainVisual({visualTheme}) {
+export interface mainVisuals {
+    1: string
+    2: string
+    3: string
+}
 
-    const visual1 = visualTheme === "visualTop" ? "/58787b52-38a8-4379-85c7-e61bb7f846c2.jpg" : "";
-    const visual2 = visualTheme === "visualTop" ? "/circle_img1.jpg" : "";
-    const visual3 = visualTheme === "visualTop" ? "/lead_img01.jpg" : "";
+export default function MainVisual({visualTheme}: {visualTheme: mainVisuals}) {
     const visualShow=""
 
     // if (visualTheme === "visualTop") {
@@ -17,17 +19,17 @@ export default function MainVisual({visualTheme}) {
       <div className="mt-6 relative main-visual overflow-hidden">
         <div className="absolute">
             <div className="main-visual__img-container rotate-3 shadow-2xl relative bg-white">
-                <img src={visual1} alt="" className="main-visual__img object-cover absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                <img src={visualTheme[1]} alt="" className="main-visual__img object-cover absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
             </div>
         </div>
         <div className="absolute right-0">
             <div className="main-visual__img-container rotate-3 shadow-2xl relative bg-white">
-                <img src={visual2} alt="" className="main-visual__img object-cover absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                <img src={visualTheme[2]} alt="" className="main-visual__img object-cover absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
             </div>
         </div>
         <div className="absolute left-1/2 transform -translate-x-1/2">
             <div className="main-visual__img-container -rotate-3 shadow-2xl relative bg-white">
-                <img src={visual3} alt="" className="main-visual__img object-cover absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                <img src={visualTheme[3]} alt="" className="main-visual__img object-cover absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
                 <p className={`absolute bottom-24 right-10 text-white text-8xl -rotate-12 main-visual__p ${visualShow}`}>わすれられない<br></br>おもいでを</p>
             </div>
         </div>
